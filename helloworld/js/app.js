7d34cc74801ddbe4b56a18a6081f5db00322efb6
Rahul Darji <rahul.darji@raweng.com>
2015-06-12 18:22:41 +0530

    Nw

---
+++ helloworld/js/app.js
@@ -0,0 +1,14 @@
+var delay = 4000; //Your delay in milliseconds
+var count=0;
+console.log(count);
+if(count==0){
+
+  window.setTimeout(function(){
+
+        // Move to a new location or you can do something else
+         window.location.href = "index.html";
+
+    }, 5000);
+
+count++;
+}
